c97cb9a0fde003e6fa0c3bbb688e9bc5218d66d0
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-05 18:15:01 +0530

    Created the Form component inside Main component.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,6 +9,7 @@ import {
 } from "@material-ui/core";
 
 import useStyles from "./styles";
+import Form from "./Form/Form";
 
 const Main = () => {
   const classes = useStyles();
@@ -27,7 +28,7 @@ const Main = () => {
           Try saying: Add income for ₹100 in Category Salary for Monday...
         </Typography>
         <Divider />
-        {/* Form */}
+        <Form />
       </CardContent>
       <CardContent className={classes.cardContent}>
         <Grid container spacing={2}>
