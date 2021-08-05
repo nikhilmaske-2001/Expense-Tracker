6cadac587d7453cc9c147f33223205dda11292dd
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-05 09:54:15 +0530

    Show three cards on the screen where Details is left and right card.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import Details from './components/Details/Details'
 const App = () => {
     return (
         <div>
-            <Grid>
-                <Grid>
+            <Grid container spacing={0} alignItems="center" justify="center" style={{height: "100vh"}}>
+                <Grid item xs={12} sm={4}>
+                 <Details/>   
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                 Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
                  <Details/>   
                 </Grid>
             </Grid>
