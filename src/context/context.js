ac345fbe105d7acdcb0de9618b47e73fa322e38f
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-06 16:20:51 +0530

    Created contextReducer.

--- src/context/context.js
+++ src/context/context.js
@@ -1,10 +1,17 @@
 import React, {useReducer, createContext} from "react";
 
+import contextReducer from "./contextReducer";
+
 const initialState = [];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
 export const Provider = ({children}) => {
+//  An alternative to useState.
+//  useReducer is usually preferable to useState when you have
+//  complex state logic that involves multiple sub-values. 
+    const {transactions, dispatch} = useReducer(contextReducer, initialState);
+
     return (
         <ExpenseTrackerContext.Provider value = {{appName: "Expense Tracker"}}>
             {children}
---
+++ src/context/contextReducer.js
@@ -0,0 +1,6 @@
+// Reducer() => a function that takes in the old state=> and an action => return new state 
+const contextReducer = (state, action) => {
+
+}
+
+export default contextReducer;
