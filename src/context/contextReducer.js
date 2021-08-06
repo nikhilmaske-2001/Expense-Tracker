16b4fdc42d1ab7fb0d8df4052f222f7a4cc1f6db
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-06 16:32:19 +0530

    Add contextReducer for DELETE and ADD transactions.

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,6 +1,16 @@
 // Reducer() => a function that takes in the old state=> and an action => return new state 
 const contextReducer = (state, action) => {
-
+    let transactions;
+    switch (action.type) {
+        case "DELETE_TRANSACTION":
+            transactions = state.filter((t) => t.id !== action.type)
+            return transactions;
+        case "ADD_TRANSACTION":
+            transactions = [action.payload, ...state];
+            return transactions;
+        default:
+            return state;
+    }
 }
 
 export default contextReducer;
