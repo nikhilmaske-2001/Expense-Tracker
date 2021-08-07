d2eea28d25934cd83e4f7870848c78a7c418493b
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-07 16:09:22 +0530

    Wrap our Provider and App in SpeechProvider.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { SpeechProvider } from "@speechly/react-client";
 
 import App from "./App";
 import "./index.css";
 import {Provider} from "./context/context";
 
-ReactDOM.render(<Provider><App/></Provider>, document.getElementById("root"));
+ReactDOM.render(
+    <SpeechProvider appId="23b66b83-e3b3-4839-a417-959e6932a0c1" language="en-US">
+        <Provider>
+            <App/>
+        </Provider>
+    </SpeechProvider>, 
+    document.getElementById("root"));
