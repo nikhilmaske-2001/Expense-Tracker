a47f7031baff4791831c99cd0a76900940c553c1
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-07 16:26:15 +0530

    Show speechContext words in Form.

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -18,6 +18,7 @@ import {
   incomeCategories,
   expenseCategories,
 } from "../../../constants/categories";
+import { useSpeechContext } from "@speechly/react-client";
 
 const initialState = {
   amount: "",
@@ -30,6 +31,8 @@ const Form = () => {
   const classes = useStyles();
   const [formData, setFormData] = useState(initialState);
   const { addTransaction } = useContext(ExpenseTrackerContext);
+  const { segment } = useSpeechContext();
+
   const createTransaction = () => {
     const transaction = {
       ...formData,
@@ -47,7 +50,7 @@ const Form = () => {
     <Grid container spacing={2}>
       <Grid item xs={12}>
         <Typography align="center" variant="subtitle2" gutterBottom>
-          ...
+          {segment ? <>{segment.words.map((w) => w.value).join(" ")}</> : null}
         </Typography>
       </Grid>
 
