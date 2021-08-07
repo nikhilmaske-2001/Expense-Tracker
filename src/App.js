8c11e38c3bfab1d13151a03a42976c37ea0a750e
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-07 16:17:07 +0530

    Add push to talk button.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from "@material-ui/core";
+import { ErrorPanel, PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
 
 import Details from './components/Details/Details';
 import Main from "./components/Main/Main";
@@ -22,6 +23,10 @@ const App = () => {
                  <Details title ="Expense"/>   
                 </Grid>
             </Grid>
+            <PushToTalkButtonContainer>
+                <PushToTalkButton/>
+                <ErrorPanel/>
+            </PushToTalkButtonContainer>
         </div>
     )
 }
