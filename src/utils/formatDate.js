30595e7821150bfa515ead7b759ce8365d4df04e
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-07 11:40:26 +0530

    Add comments for formatDate logic of adding 0 in the beginning..

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,13 +1,20 @@
+// As month index starts from 0, we are adding 1 to it.
 const formatDate = (date) =>  {
     const d = new Date(date);
     let month = `${d.getMonth() + 1}`;
     let day = `${d.getDate()}`;
     const year = d.getFullYear();
 
+    // Month can be 1-9 (Jan-Sept)
+    // We want to show 1 as 01
+    // 2 as 02, 4 as 04 ...
     if(month.length < 2) {
         month = `0${month}`;
     }
 
+    // Day can be 1-7 (Sun-Sat)
+    // We want to show 1 as 01
+    // 2 as 02, 4 as 04 ...
     if(day.length < 2) {
         day = `0${day}`;
     }
