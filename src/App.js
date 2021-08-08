54f1d6cdcaae1bdf5923b8ca1c6d0216dbec573d
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-08 12:57:27 +0530

    Switch to main context if scrolled is down.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef } from 'react';
 import { Grid } from "@material-ui/core";
 import { ErrorPanel, PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
 
@@ -6,17 +6,27 @@ import Details from './components/Details/Details';
 import Main from "./components/Main/Main";
 
 import useStyles from "./styles";
+import { SpeechState, useSpeechContext } from '@speechly/react-client';
 
 const App = () => {
     const classes = useStyles();
+    const {speechState} = useSpeechContext();
+    const main = useRef(null);
 
+    const executeScroll = () => main.current.scrollIntoView();
+
+    useEffect(() => {
+        if(speechState === SpeechState.Recording) {
+            executeScroll();
+        }
+    }, [speechState])
     return (
         <div>
             <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{height: "100vh"}}>
                 <Grid item xs={12} sm={4} className = {classes.mobile}>
                  <Details title = "Income"/>   
                 </Grid>
-                <Grid item xs={12} sm={3} className = {classes.main}>
+                <Grid ref = {main} item xs={12} sm={3} className = {classes.main}>
                  <Main/>
                 </Grid>
                 <Grid item xs={12} sm={4} className = {classes.desktop}>
