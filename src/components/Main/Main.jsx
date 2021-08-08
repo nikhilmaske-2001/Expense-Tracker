58e48063fc47cd8e1d9734689f6e82328d1141ca
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-08 12:58:32 +0530

    Add space between Divider.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -31,7 +31,7 @@ const Main = () => {
         >
           <InfoCard />
         </Typography>
-        <Divider />
+        <Divider className={classes.divider} />
         <Form />
       </CardContent>
       <CardContent className={classes.cartContent}>
