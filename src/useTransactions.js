f390d18b5d0c074f844fee1fde875625ba23cf91
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-08 13:02:24 +0530

    Remove console statements.

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -10,8 +10,6 @@ const useTransactions = (title) => {
     const total = transactionsPerType.reduce((acc, currVal) => acc += currVal.amount, 0);
     const categories = title === "Income" ? incomeCategories : expenseCategories;
 
-    console.log({transactionsPerType , total, categories});
-
     transactionsPerType.forEach((t) => {
         const category = categories.find((c) => c.type === t.category);
 
