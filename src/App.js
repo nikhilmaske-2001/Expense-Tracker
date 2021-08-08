ea4bbe916328a4068fbd263994a5985ef05afec9
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-08-08 12:50:17 +0530

    Make the website mobile responsive.

--- src/App.js
+++ src/App.js
@@ -13,13 +13,16 @@ const App = () => {
     return (
         <div>
             <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{height: "100vh"}}>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} className = {classes.mobile}>
                  <Details title = "Income"/>   
                 </Grid>
-                <Grid item xs={12} sm={3}>
+                <Grid item xs={12} sm={3} className = {classes.main}>
                  <Main/>
                 </Grid>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} className = {classes.desktop}>
+                 <Details title = "Income"/>   
+                </Grid>
+                <Grid item xs={12} sm={4} className={classes.last}>
                  <Details title ="Expense"/>   
                 </Grid>
             </Grid>
